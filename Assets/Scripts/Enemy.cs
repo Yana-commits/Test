88afe75793d8685c92de9fdb14ebe10cd02582d8
yana <kovriginayana60@gmail.com>
2021-06-04 16:18:20 +0300

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/Scripts/NavigationBaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationBaker : MonoBehaviour
+{
+    public NavMeshSurface2d surfaces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+            surfaces.BuildNavMesh();
+       
+    }
+
+}
