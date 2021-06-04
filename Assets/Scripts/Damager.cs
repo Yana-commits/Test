7d8fb343e71a28ea62fe125a2927d340f47e6994
yana <kovriginayana60@gmail.com>
2021-06-04 18:22:51 +0300

    friday commit

---
+++ Assets/Scripts/Damager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    [SerializeField]
+    private float damage;
+
+    private GameObject owner;
+    public GameObject Owner
+    {
+
+        get { return owner; }
+
+        set { owner = value; }
+
+    }
+
+    public float Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            if (value > 10)
+            {
+                damage = 10;
+            }
+            else
+            {
+                damage = value;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!GameObject.Equals(collision.gameObject, Owner))
+        {
+            Destructable target = collision.gameObject.GetComponent<Destructable>();
+
+            if (target != null)
+            {
+                
+            }
+
+            gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+    private Joystick joystick;
+    public int speed;
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rigidbody.velocity = new Vector3(-joystick.Vertical * speed, rigidbody.velocity.y, joystick.Horizontal * speed);
+    }
+}
